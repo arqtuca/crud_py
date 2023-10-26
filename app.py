346ff78028e94eb1cdd04a3f4d9feb258631a28f
Arthur Rodrigues <121184857+arqtuca@users.noreply.github.com>
2023-10-25 21:32:32 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+import swagger_ui
+
+# Adiciona a documentação Swagger à aplicação
+swagger_ui.init_app(app)
---
+++ crudmysql.py
@@ -0,0 +1,32 @@
+import swagger_ui
+
+# Adiciona a documentação Swagger à aplicação
+swagger_ui.init_app(app)
+
+import mysql.connector
+
+def conectar():
+
+  connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="011292",
+    database="bd_escola"
+  )
+
+  return connection
+
+def criar_aluno(nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala):
+
+  connection = conectar()
+  cursor = connection.cursor()
+
+  # Insere o aluno no banco de dados
+  sql = "INSERT INTO alunos (nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala) VALUES (%s, %s, %s, %s, %s, %s)"
+  val = (nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala)
+  cursor.execute(sql, val)
+
+  connection.commit()
+
+  return cursor.lastrowid
+
